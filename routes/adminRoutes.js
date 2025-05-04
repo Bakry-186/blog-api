5e8eea9e896159b2dfaaf27a73869bd3e2f6f2a6
bakry56 <abdobakry823@gmail.com>
2025-05-04 17:24:02 +0300

    chore(admin): apply user management validators to admin routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -9,12 +9,24 @@ import {
   deleteUser,
 } from "../controllers/adminCtrl.js";
 
+import {
+  getUserValidator,
+  createUserValidator,
+  updateUserValidator,
+  changePasswordValidator,
+  deleteUserValidator,
+} from "../utils/validators/adminValidator.js";
+
 const router = express.Router();
 
-router.route("/").get(getUsers).post(createUser);
+router.route("/").get(getUsers).post(createUserValidator, createUser);
 
-router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
+router
+  .route("/:id")
+  .get(getUserValidator, getUser)
+  .put(updateUserValidator, updateUser)
+  .delete(deleteUserValidator, deleteUser);
 
-router.put("/change-password/:id", changePassword);
+router.put("/change-password/:id", changePasswordValidator, changePassword);
 
 export default router;
