7259b9dcce7aaaad93e17f65da47edbdcfe91722
bakry56 <abdobakry823@gmail.com>
2025-05-04 13:20:00 +0300

    add user model

---
+++ models/userModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User name is required!"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required!"],
+      unique: [true, "Email must be unique!"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required!"],
+    },
+    role: {
+      type: String,
+      enum: ["admin", "author", "user"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
