b06976e0414759626d289da35d514a3f18b74f6c
bakry56 <abdobakry823@gmail.com>
2025-05-05 12:24:11 +0300

    feat(auth): add role-based access middleware

---
+++ middlewares/roleMiddleware.js
@@ -0,0 +1,12 @@
+import asyncHandler from "express-async-handler";
+
+import ApiError from "../utils/apiError.js";
+
+const authorizeRoles = (...roles) =>
+  asyncHandler(async (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(new ApiError("Access denied!", 403));
+    }
+  });
+
+export default authorizeRoles;
