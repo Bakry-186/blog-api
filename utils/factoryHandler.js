875c75d8d119a39d97b6261ba749f06b19ecb6c7
bakry <abdobakry823@gmail.com>
2025-05-07 10:49:49 +0300

    fix filter object

--- utils/factoryHandler.js
+++ utils/factoryHandler.js
@@ -49,13 +49,13 @@ export const getOne = (Model) =>
 
 export const getAll = (Model, modelName = "") =>
   asyncHandler(async (req, res) => {
-    let filterObj = {};
-    if (req.filterObject) {
-      filterObj = req.filterObject;
+    let filterObject = {};
+    if (req.filterObj) {
+      filterObject = req.filterObj;
     }
     const countDocuments = await Model.countDocuments();
     const apiFeatures = new ApiFeatures(
-      Model.find(filterObj),
+      Model.find(filterObject),
       qs.parse(req._parsedUrl.query)
     )
       .filter()
